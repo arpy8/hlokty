cd8ce9bf949fa11b1a891b02b6a9346485bd6476
Arpit <arpitsengar99@gmail.com>
2023-12-08 01:25:58 +0530

    add channel creation after each call

--- api/main.py
+++ api/main.py
@@ -1,13 +1,14 @@
-import os 
+import os
 from flask import Flask, request, jsonify
 import requests
 
 app = Flask(__name__)
 
 TOKEN = os.environ.get('IPG_BOT_TOKEN')
-CHANNEL_ID = os.environ.get('IPG_CHANNEL_ID')
+GUILD_ID = os.environ.get('IPG_GUILD_ID')
 
-url = f'https://discord.com/api/v10/channels/{CHANNEL_ID}/messages'
+url_channels = f'https://discord.com/api/v10/guilds/{GUILD_ID}/channels'
+url_messages = f'https://discord.com/api/v10/channels/{{channel_id}}/messages'
 
 @app.route("/")
 def index():
@@ -21,20 +22,42 @@ def send_data():
         if not data:
             return jsonify({"error": "Data not provided"}), 400
 
-        send_message(data)
+        new_channel_id = create_channel(data)
+        send_message(new_channel_id, data)
         return jsonify("sent successfully")
     except Exception as e:
         print(e)
         return jsonify({"error": "Internal server error"}), 500
 
-def send_message(message):
+def create_channel(channel_name):
+    data = {
+        "name": channel_name,
+        "type": 0  # 0 for text channel, adjust if needed
+    }
+
+    try:
+        response = requests.post(url_channels, json=data, headers={
+            'Authorization': f'Bot {TOKEN}',
+            'Content-Type': 'application/json',
+        })
+
+        if response.status_code == 201:
+            return response.json()['id']
+        else:
+            print(f"Failed to create channel. Status code: {response.status_code}, Response: {response.text}")
+            return None
+    except Exception as e:
+        print("Error creating channel:", str(e))
+        return None
+
+def send_message(channel_id, message):
     if len(message) <= 2000:
         data = {
             "content": message,
         }
 
         try:
-            response = requests.post(url, json=data, headers={
+            response = requests.post(url_messages.format(channel_id=channel_id), json=data, headers={
                 'Authorization': f'Bot {TOKEN}',
                 'Content-Type': 'application/json',
             })
@@ -44,8 +67,8 @@ def send_message(message):
         except Exception as e:
             print("Error sending message:", str(e))
     else:
-        send_message(message[:2000])
-        send_message(message[2000:])
+        send_message(channel_id, message[:2000])
+        send_message(channel_id, message[2000:])
 
 if __name__ == "__main__":
-    app.run()   
+    app.run()
